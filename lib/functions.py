76c8fa985b5f79c1b9127a842bbb344f8e0259c6
mohammed <mohammed.salad@student.moringaschool.com>
2023-12-05 21:04:00 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
-
+    print(f"Hello,{ name}!")
+greet("sadam")
 def greet_with_default(name="programmer"):
-    pass
-
+    print(f"Hello,{name}!")
+greet_programmer()
 def add(num1, num2):
-    pass
-
+    return num1+num2
+add(2,3)
 def halve(number):
-    pass
+    return number/2
+halve(3)
