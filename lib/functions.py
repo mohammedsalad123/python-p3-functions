ed82468f6c7866f3c782406fdde0a6bae7dec188
mohammed <mohammed.salad@student.moringaschool.com>
2023-12-06 11:34:04 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,18 @@
 
 def greet_programmer():
     print("Hello, programmer!")
+print(greet_programmer())
 
 def greet(name):
-    print(f"Hello,{ name}!")
-greet("sadam")
+    print(f"Hello, {name}!")
+print(greet("Guido"))
 def greet_with_default(name="programmer"):
-    print(f"Hello,{name}!")
+    print(f"Hello, {name}!")
+print(greet_with_default())
+greet_with_default()
 greet_programmer()
 def add(num1, num2):
-    return num1+num2
+    return num1 + num2
 add(2,3)
 def halve(number):
     return number/2
